6b0e287f14983b0724b3d32d2a3d407a4e626dcd
glen1166 <glen1166@outlook.com>
2021-01-26 11:45:03 +0800

    concurrent dictionary

--- ParallelPrograming/Program.cs
+++ ParallelPrograming/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-
-namespace ParallelPrograming
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        ConcurrentDictionary<string, string> dictionary = new ConcurrentDictionary<string, string>();
+
+        Task t1 = Task.Factory.StartNew(() =>
+                {
+                for (int i = 0; i < 100; ++i)
+                {
+                dictionary.TryAdd(i.ToString(), i.ToString());
+                Thread.Sleep(100);
+                }
+                });
+
+        Task t2 = Task.Factory.StartNew(() =>
+                {
+                Thread.Sleep(300);
+                foreach (var item in dictionary)
+                {
+                Console.WriteLine(item.Key + "-" + item.Value);
+                Thread.Sleep(150);
+                }
+                });
+
+        try
         {
-            Console.WriteLine("Hello World!");
+            Task.WaitAll(t1, t2);
+        }
+        catch (AggregateException ex) // No exception
+        {
+            Console.WriteLine(ex.Flatten().Message);  
         }
     }
 }
